b366346fbd8213f78a050e082baebaae8adc381d
Abu Sadat Ansari <itssadatansari@gmail.com>
2024-01-15 14:36:12 +0530

    Add some more tasks in Inbox.

--- src/js/inbox.js
+++ src/js/inbox.js
@@ -1,61 +1,21 @@
 import { createArr, storeAllTask } from "./storage";
+import { displayTaskContainer } from "./task";
+import { createTask } from "./taskCard";
 
 export function setInbox(element) {
-    element.innerHTML = `
+  element.innerHTML = `
       <div class="project-title">
         <h3>Inbox</h3>
       </div>
-        <!-- Task Card Begins -->
-        <div class="task-card-container">
-          <div class="overlay hide"></div>
-          <div class="task-card">
-            <div class="task-checkbox">
-              <input type="checkbox" name="" id="isTaskDone" />
-            </div>
-            <div class="task-info">
-              <h4>Eat an Apple</h4>
-              <p class="task-description">
-                I am going to eat a red royal gala apple from the fridge
-              </p>
-              <span class="hide">I am going to eat a red royal gala apple from the fridge</span>
-              <div class="task-additionals">
-                <div class="priority">
-                  <img src="./img/flag.svg" alt="" width="25" height="25" />
-                  <p>High</p>
-                </div>
-                <div class="task-dueDate">
-                  <img src="./img/due-date.svg" alt="" width="25" height="25" />
-                  <p>2024-01-20</p>
-                </div>
-              </div>
-            </div>
-            <div class="task-operation">
-              <div class="view-note-btn">
-                <img
-                  src="./img/view-btn.svg"
-                  alt=""
-                  width="30"
-                  height="30"
-                />
-              </div>
-              <div class="modify-btn">
-                <img src="./img/modify-btn.svg" alt="" width="30" height="30" />
-              </div>
-              <div class="delete-btn">
-                <img src="./img/delete-btn.svg" alt="" width="30" height="30" />
-              </div>
-            </div>
-          </div>
-        </div>
-        <!-- Task Card Ends -->
-      
-      <!-- ADD TASK BUTTON INSIDE DISPLAY CTN STARTS-->
       <div class="project-addTask">
         <img src="./img/add-normal.svg" alt="" width="30" height="30" />
         <p>Add Task</p>
       </div>
-      <!-- ADD TASK BUTTON INSIDE DISPLAY CTN ENDS-->
-    `
+    `;
+  displayTaskContainer(createTask("Fajr Salah", "The Prophet (ﷺ) said,The two Rakah before the dawn (Fajr) prayer are better than this world and all it contains.", "2024-01-20", "High"));
+  displayTaskContainer(createTask("Read Quran", "The Prophet (ﷺ) said, The best among you (Muslims) are those who learn the Quran and teach it.", "2024-01-20", "High"));
+  displayTaskContainer(createTask("Plan and Prepare Meals", "Take time to plan and prepare healthy meals for the day to ensure a balanced and nutritious diet.", "2024-01-15", "Medium"));
+  displayTaskContainer(createTask("Explore JavaScript Prototypes", "I'm diving into JavaScript prototypes to deepen my understanding of this crucial language feature. Starting with articles, tutorials, and official documentation, I'll cover prototype chains, constructor functions, and the prototype property. Through coding exercises and small projects, I'll apply my knowledge, engaging with online communities for support. Keeping a learning log, I aim to confidently integrate prototype-based patterns into my code by the due date.", "2024-02-28", "Medium"));
 };
 
 function saveInbox() {
