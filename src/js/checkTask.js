53661b3546b1244a3d1cf19bdf8dfe4e9289a124
Abu Sadat Ansari <itssadatansari@gmail.com>
2024-01-16 16:45:00 +0530

    Manipulate local storage when task is completed.

--- src/js/checkTask.js
+++ src/js/checkTask.js
@@ -1,6 +1,12 @@
+import { getCurrentProjectName } from "./myProject";
+import { deleteATaskFromLocalS } from "./storage";
+import { getTaskCardValues } from "./taskCard";
+
 function checkTask(e) {
     const displayCtn = document.querySelector(".display-ctn");
     const taskCtn = e.currentTarget.parentNode.parentNode.parentNode;
+    const taskTitle = getTaskCardValues(taskCtn.lastElementChild).name;
+    deleteATaskFromLocalS(getCurrentProjectName(), taskTitle);
     displayCtn.removeChild(taskCtn);
 }
 
